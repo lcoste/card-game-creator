163740443a24298436a3cda249fa5096c7b95df2
Louis Coste <costelouis96@gmail.com>
2019-09-07 16:30:57 -0400

    Created client and server examples

---
+++ README.md
@@ -0,0 +1,8 @@
+# card-game-creator
+
+### Features
+
+- Create a card games
+- Share card games
+- Play with a friend
+
---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+# ip = '76.68.141.15'
+
+HOST = '192.168.2.27'  # Standard loopback interface address (localhost)
+PORT = 4567        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
